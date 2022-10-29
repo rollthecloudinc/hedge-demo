54e7fd211aec856a7e88f53022054a79a88dccde
druid <angular.druid@gmail.com>
2022-10-29 18:20:07 -0400

    Basic functional demo of primary hedge lib api.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-alert('hello world');
+import { hedge } from '@rollthecloudinc/hedge';
+
+demoIt();
+async function demoIt() {
+
+  const h = await hedge({ service: "emissionless" }, {});
+  const { service } = await h.service();
+  const { region } = await h.region();
+  
+  console.log('service', service);
+  console.log('region', region);
+
+  const { serviceDocument } = await service.document()
+  const { regionDocument } = await region.document();
+
+  console.log('service document', serviceDocument);
+  console.log('region document', regionDocument);
+
+}
